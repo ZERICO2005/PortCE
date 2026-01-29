2d5ef81ea2d9306cc4c7854e5a58b06d3d1265ff
zerico <71151164+ZERICO2005@users.noreply.github.com>
2026-01-28 19:20:27 -0700

    abstracted memory handling

---
+++ src/PortCE_memory.hpp
@@ -0,0 +1,45 @@
+#ifndef PORTCE_MEMORY_HPP
+#define PORTCE_MEMORY_HPP
+
+#include <PortCE.h>
+
+/**
+ * @brief initializes PortCE memory
+ * @returns true on success
+ */
+bool PortCE_memory_init(void);
+
+/**
+ * @brief cleans up PortCE memory
+ */
+void PortCE_memory_destroy(void);
+
+/**
+ * @brief Reads from PortCE memory
+ * @note Reads zeros when out of range
+ */
+void PortCE_memory_read(void * dst, uint24_t offset, size_t size);
+
+/**
+ * @brief Writes to PortCE memory
+ */
+void PortCE_memory_write(const void * src, uint24_t offset, size_t size);
+
+/**
+ * @brief sets a memory region to a value
+ */
+void PortCE_memory_memset(uint24_t offset, uint8_t value, size_t size);
+
+/**
+ * @brief zeros a memory region
+ */
+inline void PortCE_memory_clear(uint24_t offset, size_t size) {
+    PortCE_memory_memset(offset, 0, size);
+}
+
+/**
+ * @brief zeros all memory regions
+ */
+void PortCE_memory_clear_all(void);
+
+#endif /* PORTCE_MEMORY_HPP */

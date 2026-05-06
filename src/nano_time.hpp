f9e574851780c04b485b23001f15bede4c3faada
zerico <71151164+ZERICO2005@users.noreply.github.com>
2026-05-06 14:09:13 -0600

    fixed const --> constexpr and C-style casts in timer code

--- src/nano_time.hpp
+++ src/nano_time.hpp
@@ -10,7 +10,7 @@ typedef int64_t nano64_t;
 static inline nano64_t getNanoTime(void) {
     struct timespec tp;
     if (clock_gettime(CLOCK_REALTIME, &tp) == 0) {
-        nano64_t nanoTime = (nano64_t)tp.tv_sec * (nano64_t)1000000000 + (nano64_t)tp.tv_nsec;
+        nano64_t nanoTime = static_cast<nano64_t>(tp.tv_sec) * static_cast<nano64_t>(1000000000) + static_cast<nano64_t>(tp.tv_nsec);
         return nanoTime;
     } else {
         perror("clock_gettime");
@@ -18,27 +18,27 @@ static inline nano64_t getNanoTime(void) {
     }
 }
 
-static inline double NANO_TO_SECONDS(nano64_t t) {
+static constexpr double NANO_TO_SECONDS(nano64_t t) {
     return static_cast<double>(t) / 1.0e9;
 }
 
-static inline double NANO_TO_FRAMERATE(nano64_t t) {
+static constexpr double NANO_TO_FRAMERATE(nano64_t t) {
     return 1.0e9 / static_cast<double>(t);
 }
 
-static inline nano64_t SECONDS_TO_NANO(double s) {
+static constexpr nano64_t SECONDS_TO_NANO(double s) {
     return static_cast<nano64_t>(s * 1.0e9);
 }
 
-static inline double SECONDS_TO_FRAMERATE(double s) {
+static constexpr double SECONDS_TO_FRAMERATE(double s) {
     return 1.0 / s;
 }
 
-static inline nano64_t FRAMERATE_TO_NANO(double f) {
+static constexpr nano64_t FRAMERATE_TO_NANO(double f) {
     return static_cast<nano64_t>(1.0e9 / f);
 }
 
-static inline double FRAMERATE_TO_SECONDS(double f) {
+static constexpr double FRAMERATE_TO_SECONDS(double f) {
     return 1.0 / f;
 }
 
